bf1d3fd993c6cdcaaedf1c4190a631942810ec83
Donald Filimon <cbkshadow@icloud.com>
2023-11-30 05:47:17 -0500

    Initial Commit

---
+++ MacaStudio/MacaStudioApp.swift
@@ -0,0 +1,17 @@
+//
+//  MacaStudioApp.swift
+//  MacaStudio
+//
+//  Created by Donald Filimon on 11/30/23.
+//
+
+import SwiftUI
+
+@main
+struct MacaStudioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
